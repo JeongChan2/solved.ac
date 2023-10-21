12395142553df5525d5156eeadf2422505d36145
JeongChan2 <jcstar55@gmail.com>
2023-10-22 00:57:01 +0900

    move

--- short.py
+++ short.py
@@ -1,5 +1,11 @@
-su = int(input())
-if (su % 4 == 0 and su % 100 != 0) or (su % 400 == 0):
-  print(1)
-else:
-  print(0)
+print("|\\_/|")
+print("|q p|   /}")
+print("( 0 )\"\"\"\\")
+print("|\"^\"`    |")
+print("||_/=\\\\__|")
+
+print("|\_/|")
+print("|q p|   /}")
+print('( 0 )"""\\')
+print('|"^"`    |')
+print("||_/=\\\__|")
