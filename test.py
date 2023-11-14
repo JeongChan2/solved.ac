c2347aede5f8cc41aa934f2f286f30d32e358a4c
JeongChan2 <jcstar55@gmail.com>
2023-11-15 00:23:34 +0900

    ASCII

--- test.py
+++ test.py
@@ -1,7 +1,29 @@
-n = int(input())
-lst = list(range(n,0,-1))
+# def security(num):
+#   su = ((ord(num)+22)-65) % 26
+#   return chr(su+65)
 
-for _ in range(n-1):
-  lst.pop()
-  lst = [lst.pop()] + lst
-print(lst[0]) 
+# sen = 'EOXH MHDQV'
+# sen2 = ''
+# for ch in sen:
+#   if ch == ' ':
+#     sen2 += ' '
+#   else:
+#     sen2 += security(ch)
+# print(sen2)
+
+def security(num, j):
+  su = ((ord(num)+j)-65) % 26
+  return chr(su+65)
+
+sen = 'HDW GLP VXP'
+sen2 = ''
+j = 0
+for _ in range(25):
+  sen2 = ''
+  j +=1
+  for ch in sen:
+    if ch == ' ':
+      sen2 += ' '
+    else:
+      sen2 += security(ch,j)
+  print(sen2)
